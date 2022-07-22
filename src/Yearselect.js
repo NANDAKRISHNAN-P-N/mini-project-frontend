c5f535b08624ceb72051b9ca4e4b7d26048cf902
NANDAKRISHNAN P N <nandakrishnanunni01@gmail.com>
2022-07-22 20:51:28 +0530

    Solved merge conflicts

--- src/Yearselect.js
+++ src/Yearselect.js
@@ -25,16 +25,18 @@ const Yearselect = () => {
         <div class="flex flex-col"></div>
         <h1 class="mb-3 text-3xl font-extrabold text-center">Choose Year</h1>
       <Link to="/Studentstat">       
-      <a href="#" class="h-10 px-5 m-2 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg hover:bg-indigo-800 h-12 px-6 m-2 text-lg inline-flex items-center justify-center">1st Year</a>
+      <div class=" text-indigo-100 transition-colors duration-150
+       bg-indigo-700 rounded-lg hover:bg-indigo-800 h-12 px-6 m-2 text-lg 
+        inline-flex items-center justify-center">1st Year</div>
         </Link> 
         <Link to="/Studentstat">
-        <a href="#" class="h-10 px-5 m-2 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg hover:bg-indigo-800 h-12 px-6 m-2 text-lg inline-flex items-center justify-center">2nd Year</a>
+        <div class=" text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg hover:bg-indigo-800 h-12 px-6 m-2 text-lg inline-flex items-center justify-center">2nd Year</div>
         </Link>
         <Link to="/Studentstat">
-        <a href="#" class="h-10 px-5 m-2 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg hover:bg-indigo-800 h-12 px-6 m-2 text-lg inline-flex items-center justify-center">3rd Year</a>
+        <div class=" text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg hover:bg-indigo-800 h-12 px-6 m-2 text-lg inline-flex items-center justify-center">3rd Year</div>
         </Link>
         <Link to="/Studentstat">
-        <a href="#" class="h-10 px-5 m-2 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg hover:bg-indigo-800 h-12 px-6 m-2 text-lg inline-flex items-center justify-center">4th Year</a>
+        <div class=" text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg hover:bg-indigo-800 h-12 px-6 m-2 text-lg inline-flex items-center justify-center">4th Year</div>
         </Link>
     </div>
      </div>
