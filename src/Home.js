01b381a90d0ba81146a8919055d6f3e0b5077347
Althaf V A <althaf.ava@gmail.com>
2022-07-28 09:41:22 +0530

    home page update

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,27 @@
 import React from 'react'
-import Header from './Header'
+import { Link } from "react-router-dom"
+import image1 from './Background.jpg';
 const Home = () => {
   return (
-    // <div className="w-screen h-screen flex items-center justify-between px-20 bg-stone-500">
-    //   <div className="w-full content-start">
-    //     <Header/>
-    //   </div>
-       
-    //     <div className="h-64 flex-items-center justify-between px-20 py-20 rounded-md bg-amber-500 ">
-    //        <h1 className="text-3xl text-center text-blue-800 font-bold underline ">
-    //                ACTIVITY POINT MANAGEMENT SYSTEM
-    //        </h1>
-    //     </div>
-    // </div>
-    <div className="h-screen flex justify-center  bg-gray-600">
-      <div className="mt-5">
-      <Header/>
-      </div>
-      <div className="flex justify-center items-center mt-36 h-64 rounded-md bg-blue-500">
-        <h1 className="text-3xl text-center text-blue-800 font-bold underline">
-        ACTIVITY POINT MANAGEMENT SYSYTEM </h1>
+    <section class="h-screen bg-indigo-300">
+    <div class="flex h-full w-full items-center justify-center container mx-auto px-8">
+      <div>
+    <img src={image1}/>
+    </div>
+      <div class="max-w-2xl text-center">
+        <h1 class="text-3xl text-center text-blue-800 font-serif font-semibold tracking-wider">ACTIVITY  POINT  MANAGEMENT  SYSTEM</h1>
+  
+        <p class="mt-6 lg:text-sm text-blue-600">Manage all your college activity points under one roof</p>
+  
+        <div class="mt-8 flex flex-col space-y-3 sm:-mx-2 sm:flex-row sm:justify-center sm:space-y-0">
+        <Link to='/StudentLogin'>
+          <button class="italic transform rounded-md bg-blue-700 px-8 py-2 text-sm font-medium capitalize tracking-wide text-white transition-colors duration-200 hover:bg-blue-600 focus:bg-blue-600 focus:outline-none sm:mx-2">Get started</button>
+          </Link>
+         </div>
       </div>
     </div>
+    
+  </section>
   )
 }
 
